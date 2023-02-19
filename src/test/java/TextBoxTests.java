c3b218357cb3b9e2cd302997361633eaab9e7070
Alex Shavlyuk <1821413@gmail.com>
2023-02-19 18:42:22 +0300

    add subjects

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -21,8 +22,15 @@ public class TextBoxTests {
         $("#firstName").setValue("Alex");
         $("#lastName").setValue("Shav");
         $("#userEmail").setValue("AlexShav@mmmail.com");
-        $("#gender-radio-1").click();
-        $("#submit").click();
+        $("#gender-radio-1").sendKeys(" ");
+        $("#userNumber").setValue("+375291112233");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-dropdown-container").$(byText("December")).click();
+        $(".react-datepicker__year-dropdown-container").$(byText("1986")).click();
+        $(".react-datepicker__month-container").$(byText("24")).click();
+        $("#subjectsInput").setValue("History").pressEnter();
+
+
 
         $("#output").shouldHave(text("Alex Egorov"), text("alex@egorov.com"),
                 text("Some address 1"), text("Another address 1"));
