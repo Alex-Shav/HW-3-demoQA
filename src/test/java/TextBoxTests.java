9319aaa04525d01edd3665b40b11a147400c3dcb
Alex Shavlyuk <1821413@gmail.com>
2023-02-20 15:55:59 +0300

    add

--- src/test/java/TextBoxTests.java
+++ src/test/java/TextBoxTests.java
@@ -3,6 +3,8 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
@@ -25,12 +27,16 @@ public class TextBoxTests {
         $("#gender-radio-1").sendKeys(" ");
         $("#userNumber").setValue("+375291112233");
         $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-dropdown-container").$(byText("December")).click();
-        $(".react-datepicker__year-dropdown-container").$(byText("1986")).click();
-        $(".react-datepicker__month-container").$(byText("24")).click();
+        $("#dateOfBirthInput").setValue("24 Dec 1986").pressEnter();
         $("#subjectsInput").setValue("History").pressEnter();
-
-
+        $("#hobbies-checkbox-1").sendKeys(" ");
+        $("#uploadPicture").uploadFile(new File("src/test/resources/Pic.png"));
+        $("#currentAddress").setValue("Minsk");
+        $("#state").click();
+        $("#react-select-3-input").pressEnter();
+        $("#city").click();
+        $("#react-select-4-input").pressEnter();
+        $("#submit").click();
 
 
     }
